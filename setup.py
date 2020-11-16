4b4615932b1be88203e4339b7c9deabfa48cc15c
Julien M. (In Geo Veritas) <dev@ingeoveritas.com>
2020-11-16 15:20:27 +0100

    Enable Python 3.9

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
