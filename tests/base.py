6a72454bca4d19628ceb681244d851ff96d020fe
GeoJulien <dev@ingeoveritas.com>
2021-04-25 13:33:33 +0200

    Homogeneize doctsrtings

--- tests/base.py
+++ tests/base.py
@@ -27,25 +27,28 @@ from mkdocs.config import load_config
 class BaseTest(unittest.TestCase):
     """Base test class to be inherited by MkDocs plugins tests."""
 
-    @staticmethod
-    def get_plugin_config_from_mkdocs(mkdocs_path: str, plugin_name: str) -> dict:
+    def get_plugin_config_from_mkdocs(
+        self, mkdocs_yml_filepath: Path, plugin_name: str
+    ) -> dict:
         """Load a mkdocs.yml and returns the configuration for the specified plugin.
 
-        Args:
-            mkdocs_path (str): path to MkDocs configuration file
+        :param mkdocs_yml_filepath: path to MkDocs configuration file
+        :type mkdocs_yml_filepath: Path
+        :param plugin_name: plugin name (as mentionned into the mkdocs.yml)
+        :type plugin_name: str
 
-        Returns:
-            dict: plugin configuration loaded by MkDocs. Empty if specified plugin is \
-            not enabled into the mkdocs.yml.
+        :return: plugin configuration loaded by MkDocs. Empty if specified plugin is \
+        not enabled into the mkdocs.yml.
+        :rtype: dict
         """
         # instanciate plugin
-        cfg_mkdocs = load_config(mkdocs_path)
+        cfg_mkdocs = load_config(str(mkdocs_yml_filepath.resolve()))
 
         plugins = cfg_mkdocs.get("plugins")
         if "rss" not in plugins:
             logging.warning(
                 f"Plugin {plugin_name} is not part of enabled plugin in the MkDocs "
-                "configuration file: {mkdocs_path}"
+                "configuration file: {mkdocs_yml_filepath}"
             )
             return {}
         plugin_loaded = plugins.get("rss")
@@ -55,42 +58,58 @@ class BaseTest(unittest.TestCase):
 
         return plugin_loaded.config
 
-    def build_docs_setup(self, testproject_path: Path, mkdocs_yml_filepath: Path):
-        """
-        Runs the `mkdocs build` command
+    def build_docs_setup(
+        self, testproject_path: Path, mkdocs_yml_filepath: Path, output_path: Path
+    ):
+        """Runs the `mkdocs build` command.
 
-        Args:
-            testproject_path (Path): Path to test project
+        :param testproject_path: Path to test project
+        :type testproject_path: Path
+        :param mkdocs_yml_filepath: filepath to the MkDocs configuration file (.yml) \
+        passed as parameter to the `--config-file` argument.
+        :type mkdocs_yml_filepath: Path
+        :param output_path: folder path where to store the built website \
+        passed as parameter to the `--site-dir` argument.
+        :type output_path: Path
 
-        Returns:
-            command: Object with results of command
+        :return: Object with results of command
+        :rtype: click.testing.Result
         """
         try:
             runner = CliRunner()
-            run = runner.invoke(build_command, ["--clean", "--config-file"])
+            run = runner.invoke(
+                build_command,
+                [
+                    "--clean",
+                    "--config-file",
+                    f"{mkdocs_yml_filepath}",
+                    "--site-dir",
+                    f"{output_path}",
+                    "--verbose",
+                ],
+            )
             return run
         except Exception as err:
             logging.critical(err)
-            raise
+            return False
 
     def setup_clean_mkdocs_folder(
-        self, mkdocs_yml_path: Path, output_path: Path
+        self, mkdocs_yml_filepath: Path, output_path: Path
     ) -> Path:
+        """Sets up a clean mkdocs directory:
+
+            outputpath/testproject
+            ├── docs/
+            └── mkdocs.yml
+
+        :param mkdocs_yml_filepath: Path of mkdocs.yml file to use
+        :type mkdocs_yml_filepath: Path
+        :param output_path: Path of folder in which to create mkdocs project
+        :type output_path: Path
+
+        :return: Path to test project
+        :rtype: Path
         """
-        Sets up a clean mkdocs directory
-
-        outputpath/testproject
-        ├── docs/
-        └── mkdocs.yml
-
-        Args:
-            mkdocs_yml_path (Path): Path of mkdocs.yml file to use
-            output_path (Path): Path of folder in which to create mkdocs project
-
-        Returns:
-            testproject_path (Path): Path to test project
-        """
-
         testproject_path = output_path / "testproject"
 
         # Create empty 'testproject' folder
@@ -103,21 +122,21 @@ class BaseTest(unittest.TestCase):
 
         # Copy correct mkdocs.yml file and our test 'docs/'
         shutil.copytree("tests/fixtures/docs", testproject_path / "docs")
-        shutil.copyfile(mkdocs_yml_path, testproject_path / "mkdocs.yml")
+        shutil.copyfile(mkdocs_yml_filepath, testproject_path / "mkdocs.yml")
 
         return testproject_path
 
-    def validate_mkdocs_file(self, temp_path: str, mkdocs_yml_file: str):
+    def validate_mkdocs_file(self, temp_path: Path, mkdocs_yml_filepath: str):
         """
         Creates a clean mkdocs project
         for a mkdocs YML file, builds and validates it.
 
         Args:
             temp_path (PosixPath): Path to temporary folder
-            mkdocs_yml_file (PosixPath): Path to mkdocs.yml file
+            mkdocs_yml_filepath (PosixPath): Path to mkdocs.yml file
         """
-        testproject_path = setup_clean_mkdocs_folder(
-            mkdocs_yml_path=mkdocs_yml_file, output_path=temp_path
+        testproject_path = self.setup_clean_mkdocs_folder(
+            mkdocs_yml_filepath=mkdocs_yml_filepath, output_path=temp_path
         )
         # setup_commit_history(testproject_path)
         # result = build_docs_setup(testproject_path)
@@ -125,7 +144,7 @@ class BaseTest(unittest.TestCase):
 
         # # validate build with locale retrieved from mkdocs config file
         # validate_build(
-        #     testproject_path, plugin_config=get_plugin_config_from_mkdocs(mkdocs_yml_file)
+        #     testproject_path, plugin_config=get_plugin_config_from_mkdocs(mkdocs_yml_filepath)
         # )
 
         return testproject_path
@@ -136,9 +155,15 @@ class BaseTest(unittest.TestCase):
 # ##################################
 if __name__ == "__main__":
     base = BaseTest()
-    plg_cfg = base.get_plugin_config_from_mkdocs("mkdocs.yml", "rss")
-    # print(plg_cfg)
-    truc = base.setup_clean_mkdocs_folder(
-        mkdocs_yml_path=Path("mkdocs.yml"), output_path=Path("tests/fixtures/")
-    )
-    print(truc)
+    plg_cfg = base.get_plugin_config_from_mkdocs(Path("mkdocs.yml"), "rss")
+    print(plg_cfg)
+    # truc = base.setup_clean_mkdocs_folder(
+    #     mkdocs_yml_filepath=Path("mkdocs.yml"), output_path=Path("tests/fixtures/")
+    # )
+    # print(truc)
+    # run_result = base.build_docs_setup(
+    #     testproject_path="docs",
+    #     mkdocs_yml_filepath=Path("mkdocs.yml"),
+    #     output_path=Path("zoubi"),
+    # )
+    # print(type(run_result), run_result.exit_code)
