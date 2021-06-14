baff411f519f3972418e34b77c0165df92511883
Julien <dev@ingeoveritas.com>
2021-06-14 16:08:22 +0200

    Bump mkdocs to 1.2 (#76)
    * Bump mkdocs to 1.2
    
    * Bump version
    
    * Update main branch
    
    * Fix docker with build-base since material removed it
    
    * typo
    
    * Install current version
    
    * Make docker lighter

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires=[
         "GitPython==3.1.*",
         "Jinja2>=2,<3",
-        "mkdocs>=1.1,<1.2",
+        "mkdocs>=1.1,<1.3",
     ],
     # metadata
     keywords="mkdocs rss git plugin",
