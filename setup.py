978fdbc2ac435f40c7d68e61c2cc113374261b7d
GeoJulien <dev@ingeoveritas.com>
2022-02-10 20:58:13 +0100

    Enable Python 3.10

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
