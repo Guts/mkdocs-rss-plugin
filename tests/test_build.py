453a9c0a4a94cfc4b275c7c00ebc40ef6c93dd71
GeoJulien <dev@ingeoveritas.com>
2022-03-29 19:29:34 +0200

    Test against minimal configuration

--- tests/test_build.py
+++ tests/test_build.py
@@ -66,6 +66,22 @@ class TestBuildRss(BaseTest):
                 testproject_path="docs",
                 mkdocs_yml_filepath=Path("mkdocs.yml"),
                 output_path=tmpdirname,
+                strict=False,
+            )
+
+            if cli_result.exception is not None:
+                e = cli_result.exception
+                logger.debug(format_exception(type(e), e, e.__traceback__))
+
+            self.assertEqual(cli_result.exit_code, 0)
+            self.assertIsNone(cli_result.exception)
+
+    def test_simple_build_minimal(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            cli_result = self.build_docs_setup(
+                testproject_path="docs",
+                mkdocs_yml_filepath=Path("tests/fixtures/mkdocs_minimal.yml"),
+                output_path=tmpdirname,
                 strict=True,
             )
 
@@ -75,6 +91,23 @@ class TestBuildRss(BaseTest):
 
             self.assertEqual(cli_result.exit_code, 0)
             self.assertIsNone(cli_result.exception)
+
+            # created items
+            feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_created.xml")
+            for feed_item in feed_parsed.entries:
+                print(feed_item.keys())
+                # mandatory properties
+                self.assertTrue("description" in feed_item)
+                self.assertTrue("guid" in feed_item)
+                self.assertTrue("link" in feed_item)
+                self.assertTrue("published" in feed_item)
+                self.assertTrue("source" in feed_item)
+                self.assertTrue("title" in feed_item)
+                # optional - following should not be present in the feed by default
+                self.assertTrue("author" not in feed_item)
+                self.assertTrue("category" not in feed_item)
+                self.assertTrue("comments" not in feed_item)
+                self.assertTrue("enclosure" not in feed_item)
 
     def test_simple_build_disabled(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -108,13 +141,33 @@ class TestBuildRss(BaseTest):
 
             # created items
             feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_created.xml")
-            self.assertEqual(feed_parsed.bozo, 0)
+            self.assertEqual(len(feed_parsed.entries), 3)
 
             # updated items
             feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_updated.xml")
-            self.assertEqual(feed_parsed.bozo, 0)
-
             self.assertEqual(len(feed_parsed.entries), 3)
+
+    def test_simple_build_item_categories_enabled(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            cli_result = self.build_docs_setup(
+                testproject_path="docs",
+                mkdocs_yml_filepath=Path("tests/fixtures/mkdocs_item_categories.yml"),
+                output_path=tmpdirname,
+                strict=True,
+            )
+            if cli_result.exception is not None:
+                e = cli_result.exception
+                logger.debug(format_exception(type(e), e, e.__traceback__))
+
+            self.assertEqual(cli_result.exit_code, 0)
+            self.assertIsNone(cli_result.exception)
+
+            # created items
+            feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_created.xml")
+
+            for feed_item in feed_parsed.entries:
+                if feed_item.title in ("Test page with meta",):
+                    self.assertTrue("category" in feed_item)
 
     def test_simple_build_item_comments_enabled(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
