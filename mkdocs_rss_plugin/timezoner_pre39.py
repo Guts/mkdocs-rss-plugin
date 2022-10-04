9b217868c365af29f19ed3bad0b554a0def1d6b9
GeoJulien <dev@ingeoveritas.com>
2022-10-04 12:14:33 +0200

    timezoner for python < 3.9

--- mkdocs_rss_plugin/timezoner_pre39.py
+++ mkdocs_rss_plugin/timezoner_pre39.py
@@ -2,8 +2,8 @@
 
 
 """
-    Manage timezones for pages date(time)s using zoneinfo module, added in Python 3.9.
-
+    Manage timezones for pages date(time)s using pytz module.
+    Meant to be dropped when Python 3.8 reaches EOL.
 """
 
 # ############################################################################
@@ -14,6 +14,9 @@
 import logging
 from datetime import datetime
 from functools import lru_cache
+
+# 3rd party
+import pytz
 
 # ############################################################################
 # ########## Globals #############
@@ -28,17 +31,24 @@ logger = logging.getLogger("mkdocs.mkdocs_rss_plugin")
 # ################################
 
 
-@lru_cache(typed=True)
 def set_datetime_zoneinfo(
-    input_datetime: datetime, config_timezone: str = None
+    input_datetime: datetime, config_timezone: str = "UTC"
 ) -> datetime:
-    """_summary_
+    """Apply timezone to a naive datetime.
 
-    :param input_datetime: _description_
+    :param input_datetime: offset-naive datetime
     :type input_datetime: datetime
-    :param config_timezone: _description_
-    :type config_timezone: str
-    :return: _description_
+    :param config_timezone: name of timezone as registered in IANA database,
+    defaults to "UTC". Example : Europe/Paris.
+    :type config_timezone: str, optional
+
+    :return: offset-aware datetime
     :rtype: datetime
     """
-    pass
+    if input_datetime.tzinfo:
+        return input_datetime
+    elif not config_timezone:
+        return input_datetime.replace(tzinfo=pytz.utc)
+    else:
+        config_tz = pytz.timezone(config_timezone)
+        return input_datetime.replace(tzinfo=config_tz)
