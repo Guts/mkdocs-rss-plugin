739afd22af25ef7342de981706062714d5aaadb6
GeoJulien <dev@ingeoveritas.com>
2024-12-02 16:39:50 +0100

    feature(material_blog): extend tests against new blog integration

--- tests/test_integrations_material_blog.py
+++ tests/test_integrations_material_blog.py
@@ -14,11 +14,14 @@
 # ##################################
 
 # Standard library
+import tempfile
 import unittest
 from logging import DEBUG, getLogger
 from pathlib import Path
+from traceback import format_exception
 
 # 3rd party
+import feedparser
 from mkdocs.config import load_config
 
 # package
@@ -41,6 +44,24 @@ class TestRssPluginIntegrationsMaterialBlog(BaseTest):
     """Test integration of Material Blog plugin with RSS plugin."""
 
     # -- TESTS ---------------------------------------------------------
+    def test_plugin_config_social_cards_enabled_but_integration_disabled(self):
+        # default reference
+        cfg_mkdocs = load_config(
+            str(
+                Path(
+                    "tests/fixtures/mkdocs_items_material_blog_enabled_but_integration_disabled.yml"
+                ).resolve()
+            )
+        )
+
+        integration_social_cards = IntegrationMaterialBlog(
+            mkdocs_config=cfg_mkdocs,
+            switch_force=cfg_mkdocs.plugins.get("rss").config.use_material_blog,
+        )
+        self.assertTrue(integration_social_cards.IS_THEME_MATERIAL)
+        self.assertTrue(integration_social_cards.IS_BLOG_PLUGIN_ENABLED)
+        self.assertFalse(integration_social_cards.IS_ENABLED)
+
     def test_plugin_config_blog_enabled(self):
         # default reference
         cfg_mkdocs = load_config(
@@ -51,6 +72,57 @@ class TestRssPluginIntegrationsMaterialBlog(BaseTest):
         self.assertTrue(integration_social_cards.IS_THEME_MATERIAL)
         self.assertTrue(integration_social_cards.IS_BLOG_PLUGIN_ENABLED)
         self.assertTrue(integration_social_cards.IS_ENABLED)
+
+    def test_simple_build(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            cli_result = self.build_docs_setup(
+                testproject_path="docs",
+                mkdocs_yml_filepath=Path(
+                    "tests/fixtures/mkdocs_items_material_blog_enabled.yml"
+                ),
+                output_path=tmpdirname,
+                strict=False,
+            )
+
+            if cli_result.exception is not None:
+                e = cli_result.exception
+                logger.debug(format_exception(type(e), e, e.__traceback__))
+
+            self.assertEqual(cli_result.exit_code, 0)
+            self.assertIsNone(cli_result.exception)
+
+            # created items
+            feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_created.xml")
+            self.assertEqual(feed_parsed.bozo, 0)
+
+            # updated items
+            feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_updated.xml")
+            self.assertEqual(feed_parsed.bozo, 0)
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            cli_result = self.build_docs_setup(
+                testproject_path="docs",
+                mkdocs_yml_filepath=Path(
+                    "tests/fixtures/mkdocs_items_material_blog_enabled_but_integration_disabled.yml"
+                ),
+                output_path=tmpdirname,
+                strict=False,
+            )
+
+            if cli_result.exception is not None:
+                e = cli_result.exception
+                logger.debug(format_exception(type(e), e, e.__traceback__))
+
+            self.assertEqual(cli_result.exit_code, 0)
+            self.assertIsNone(cli_result.exception)
+
+            # created items
+            feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_created.xml")
+            self.assertEqual(feed_parsed.bozo, 0)
+
+            # updated items
+            feed_parsed = feedparser.parse(Path(tmpdirname) / "feed_rss_updated.xml")
+            self.assertEqual(feed_parsed.bozo, 0)
 
 
 # ##############################################################################
